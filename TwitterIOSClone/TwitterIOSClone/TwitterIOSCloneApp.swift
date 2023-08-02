91e9b4fc69efe013304de4d25d6c395640f58745
Ahmad <ausman0125@gmail.com>
2023-08-01 17:54:57 -0700

    init commit

---
+++ TwitterIOSClone/TwitterIOSClone/TwitterIOSCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterIOSCloneApp.swift
+//  TwitterIOSClone
+//
+//  Created by Ahmad Usman on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterIOSCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
